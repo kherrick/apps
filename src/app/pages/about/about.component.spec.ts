a51fab7c8772550ef3756b451893203f389e2920
Karl Herrick <me@karlherrick.com>
2023-01-14 22:10:07 -0600

    link to home from about page

--- src/app/pages/about/about.component.spec.ts
+++ src/app/pages/about/about.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AboutComponent } from './about.component';
 
@@ -8,7 +9,7 @@ describe('AboutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ AboutComponent ]
+      imports: [ AboutComponent, RouterTestingModule ]
     })
     .compileComponents();
 
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <ng-container>
       about works!
+
+      <a [routerLink]="'/'">home</a>
     </ng-container>
   `,
   styles: [
