35969130785463ca1a689dcafce8bbf5f843f6e9
Karl Herrick <me@karlherrick.com>
2023-01-15 13:51:58 -0600

    add progress-container and use material-design-lite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID, ViewEncapsulation } from '@angular/core';
 
 @Component({
   encapsulation: ViewEncapsulation.ShadowDom,
@@ -12,4 +13,22 @@ import { Component, ViewEncapsulation } from '@angular/core';
   `,
   styles: []
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  private isBrowser: boolean;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
+
+  ngOnInit(): void {
+    const progress: HTMLProgressElement | null =
+      this.document.querySelector('.progress');
+
+    if (progress && this.isBrowser) {
+      progress.style.display = 'none';
+    }
+  }
+}
