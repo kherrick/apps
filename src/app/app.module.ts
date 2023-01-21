eec578b1669696c866d63d621aedbee506d9f423
Karl Herrick <me@karlherrick.com>
2023-01-21 10:18:11 -0600

    npx ng add @angular/pwa --project news --skip-confirmation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { ShellComponent } from './shell/shell/shell.component';
 
 import { environment } from 'src/environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -14,6 +15,12 @@ import { environment } from 'src/environments/environment';
     BrowserModule.withServerTransition({ appId: 'apps' }),
     AppRoutingModule,
     ShellComponent,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [{ provide: APP_BASE_HREF, useValue: environment.APP_BASE_HREF }],
   bootstrap: [AppComponent],
